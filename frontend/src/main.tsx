8519a96847575d2aaf984208d2b0cdaa1a725e3c
yowunpansilu <pansilu03@gmail.com>
2026-05-03 23:39:30 +0530

    defefw

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,9 +4,11 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 import './index.css'
 import App from './App.tsx'
 
+const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID ?? ''
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+    <GoogleOAuthProvider clientId={googleClientId}>
       <App />
     </GoogleOAuthProvider>
   </StrictMode>,
